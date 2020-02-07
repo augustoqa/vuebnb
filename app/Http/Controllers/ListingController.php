1fc1e8bccd846f0849d63e5a92c095ab8185c366
Cesar Acual <chechaacual@gmail.com>
2020-02-07 10:02:19 -0600

    Refactoring

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -15,19 +15,32 @@ class ListingController extends Controller
 		return $model;
 	}
 
+	private function get_listing($listing)
+	{
+		$model = $listing->toArray();
+		for ($i = 1; $i <= 4; $i++) {
+			$model["image_{$i}"] = asset("images/{$listing->id}/Image_{$i}.jpg");
+		}
+
+		return collect(['listing' => $model]);
+	}
+
     public function get_listing_api(Listing $listing)
     {
-    	$model = $listing->toArray();
-    	$model = $this->add_image_urls($model, $listing->id);
+		$data = $this->get_listing($listing);
 
-    	return response()->json($model);
+    	return response()->json($data);
 	}
 	
 	public function get_listing_web(Listing $listing)
 	{
-		$model = $listing->toArray();
-		$model = $this->add_image_urls($model, $listing->id);
+		$data = $this->get_listing($listing);
 		
-		return view('app', compact('model'));
+		return view('app', compact('data'));
+	}
+
+	public function get_home_web()
+    {
+        return view('app', ['data' => []]);
 	}
 }
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,5 @@
 <?php
 
+Route::get('/', 'ListingController@get_home_web');
+
 Route::get('/listing/{listing}', 'ListingController@get_listing_web');
